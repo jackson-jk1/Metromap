00c43d163fbea9a90fab51a3dc8b85d8925ddf33
jackson-jk1 <jackson.longo.santos@gmail.com>
2024-09-13 16:30:04 -0300

    main

---
+++ assets/js/main.js
@@ -0,0 +1,205 @@
+/**
+* Template Name: UpConstruction
+* Template URL: https://bootstrapmade.com/upconstruction-bootstrap-construction-website-template/
+* Updated: Aug 07 2024 with Bootstrap v5.3.3
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+
+(function() {
+  (function() {
+    emailjs.init("ZKdTM1cWTPHYUuuYR"); // Substitua "YOUR_PUBLIC_KEY" pela chave pública do EmailJS
+  })();
+  "use strict";
+  const mobileNavToggle = document.querySelector('#navToggle');
+  const body = document.querySelector('body');
+
+  mobileNavToggle.addEventListener('click', function () {
+      body.classList.toggle('mobile-nav-active');
+      mobileNavToggle.classList.toggle('bi-list');
+      mobileNavToggle.classList.toggle('bi-x');
+  });
+
+  // Função para rolar até a seção específica
+  const links = document.querySelectorAll('.nav-link');
+
+  links.forEach(link => {
+      link.addEventListener('click', function (e) {
+          e.preventDefault();
+          const sectionId = this.getAttribute('data-section');
+          const section = document.querySelector(sectionId);
+
+          if (section) {
+              window.scrollTo({
+                  top: section.offsetTop - 50, // Compensação para o header fixo
+                  behavior: 'smooth'
+              });
+          }
+
+          if (body.classList.contains('mobile-nav-active')) {
+              body.classList.remove('mobile-nav-active');
+              mobileNavToggle.classList.add('bi-list');
+              mobileNavToggle.classList.remove('bi-x');
+          }
+      });
+  });
+
+  // Função para gerenciar a classe "active" no menu com base na rolagem
+  window.addEventListener('scroll', function () {
+      const scrollPosition = window.scrollY;
+      links.forEach(link => {
+          const sectionId = link.getAttribute('data-section');
+          const section = document.querySelector(sectionId);
+          
+          if (section && scrollPosition >= section.offsetTop - 60 && scrollPosition < section.offsetTop + section.offsetHeight) {
+              links.forEach(l => l.classList.remove('active'));
+              link.classList.add('active');
+          }
+      });
+  });
+  /**
+   * Apply .scrolled class to the body as the page is scrolled down
+   */
+  function toggleScrolled() {
+    const selectBody = document.querySelector('body');
+    const selectHeader = document.querySelector('#header');
+    if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
+    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+  }
+
+  document.addEventListener('scroll', toggleScrolled);
+  window.addEventListener('load', toggleScrolled);
+
+  /**
+   * Hide mobile nav on same-page/hash links
+   */
+  document.querySelectorAll('#navmenu a').forEach(navmenu => {
+    navmenu.addEventListener('click', () => {
+      if (document.querySelector('.mobile-nav-active')) {
+        mobileNavToogle();
+      }
+    });
+
+  });
+
+  /**
+   * Toggle mobile nav dropdowns
+   */
+  document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
+    navmenu.addEventListener('click', function(e) {
+      e.preventDefault();
+      this.parentNode.classList.toggle('active');
+      this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+      e.stopImmediatePropagation();
+    });
+  });
+
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
+
+  /**
+   * Scroll top button
+   */
+  let scrollTop = document.querySelector('.scroll-top');
+
+  function toggleScrollTop() {
+    if (scrollTop) {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+  }
+  scrollTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+
+  window.addEventListener('load', toggleScrollTop);
+  document.addEventListener('scroll', toggleScrollTop);
+
+  /**
+   * Animation on scroll function and init
+   */
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
+
+  /**
+   * Initiate glightbox
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Init isotope layout and filters
+   */
+  document.querySelectorAll('.isotope-layout').forEach(function(isotopeItem) {
+    let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
+    let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
+    let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
+
+    let initIsotope;
+    imagesLoaded(isotopeItem.querySelector('.isotope-container'), function() {
+      initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
+        itemSelector: '.isotope-item',
+        layoutMode: layout,
+        filter: filter,
+        sortBy: sort
+      });
+    });
+
+    isotopeItem.querySelectorAll('.isotope-filters li').forEach(function(filters) {
+      filters.addEventListener('click', function() {
+        isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
+        this.classList.add('filter-active');
+        initIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        if (typeof aosInit === 'function') {
+          aosInit();
+        }
+      }, false);
+    });
+
+  });
+
+  /**
+   * Init swiper sliders
+   */
+  function initSwiper() {
+    document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+      let config = JSON.parse(
+        swiperElement.querySelector(".swiper-config").innerHTML.trim()
+      );
+
+      if (swiperElement.classList.contains("swiper-tab")) {
+        initSwiperWithCustomPagination(swiperElement, config);
+      } else {
+        new Swiper(swiperElement, config);
+      }
+    });
+  }
+
+  window.addEventListener("load", initSwiper);
+
+  /**
+   * Initiate Pure Counter
+   */
+  new PureCounter();
+
+})();
---
+++ assets/vendor/php-email-form/validate.js
@@ -0,0 +1,114 @@
+
+  (function () {
+    "use strict";
+    $("input[name='phone']").inputmask("(99) 99999-9999");
+    
+    // Máscara para o campo de email
+    $("input[name='email']").inputmask({
+        alias: "email",
+        placeholder: "_"
+    });
+
+    let forms = document.querySelectorAll('.php-email-form');
+
+    forms.forEach(function (e) {
+      e.addEventListener('submit', function (event) {
+        event.preventDefault();
+  
+        let thisForm = this;
+        let action = thisForm.getAttribute('action');
+        let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+  
+        if (!action) {
+          displayError(thisForm, 'The form action property is not set!');
+          return;
+        }
+  
+        let recaptchaResponse = grecaptcha.getResponse();
+  
+        if (!recaptchaResponse) {
+          displayError(thisForm, 'Please complete the reCAPTCHA.');
+          return;
+        }
+  
+        thisForm.querySelector('.loading').classList.add('d-block');
+        thisForm.querySelector('.error-message').classList.remove('d-block');
+        thisForm.querySelector('.sent-message').classList.remove('d-block');
+  
+        let formData = new FormData(thisForm);
+        formData.append('recaptcha-response', recaptchaResponse);
+  
+        emailjs.init("YOUR_PUBLIC_KEY");  // Replace with your public key
+  
+        emailjs.send("GmailService", "template_pkbg55q", {
+          name: formData.get('name'),
+          email: formData.get('email'),
+          phone: formData.get('phone'),
+          message: formData.get('message'),
+          'recaptcha-response': recaptchaResponse
+        }).then(() => {
+          thisForm.querySelector('.loading').classList.remove('d-block');
+          thisForm.querySelector('.sent-message').classList.add('d-block');
+          thisForm.reset();
+          grecaptcha.reset(); // Reset reCAPTCHA
+        }).catch((error) => {
+          console.error('EmailJS error:', error);
+          displayError(thisForm, 'Failed to send the email. Please try again later.');
+        });
+      });
+    });
+    // Função de validação personalizada com mensagens de erro visuais
+    function validateForm(thisForm, name, email, phone, message) {
+      let valid = true;
+      const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+      const phonePattern = /^(?:(?:\+|00)55\s?)?(?:\(?[1-9][0-9]\)?\s?)?(?:9[0-9]{4}-?[0-9]{4}|[0-9]{4}-?[0-9]{4})$/;
+
+      if (name === "") {
+        displayFieldError(thisForm, 'name', 'Por favor, prrencha seu nome.');
+        valid = false;
+      }
+
+      if (!emailPattern.test(email)) {
+        displayFieldError(thisForm, 'email', 'Por favor, insira um email valido.');
+        valid = false;
+      }
+
+      if (!phonePattern.test(phone)) {
+        displayFieldError(thisForm, 'phone', 'Por favor, insira um telefone de contato valido.');
+        valid = false;
+      }
+
+      if (message === "") {
+        displayFieldError(thisForm, 'message', 'Por favor, explique o motivo do contato');
+        valid = false;
+      }
+
+      return valid;
+    }
+
+    // Função para exibir mensagens de erro ao lado de cada campo
+    function displayFieldError(thisForm, fieldName, errorMessage) {
+      let errorElement = thisForm.querySelector('.error-message-' + fieldName);
+      errorElement.innerHTML = errorMessage;
+      errorElement.classList.add('d-block');
+    }
+
+    function displayError(thisForm, error) {
+      thisForm.querySelector('.loading').classList.remove('d-block');
+      thisForm.querySelector('.error-message').innerHTML = error;
+      thisForm.querySelector('.error-message').classList.add('d-block');
+      
+      // Limpa as mensagens de erro após 30 segundos
+      setTimeout(() => {
+        clearErrorMessages(thisForm);
+      }, 30000); // 30000 milissegundos = 30 segundos
+    }
+  
+    function clearErrorMessages(thisForm) {
+      thisForm.querySelectorAll('.error-message').forEach(function(el) {
+        el.classList.remove('d-block');
+        el.innerHTML = '';
+      });
+    }
+
+  })();
